5a203d2a5e94137bae1c8665b882362e59ad98dd
沈唁 <52o@qq52o.cn>
2019-01-28 17:12:59 +0800

    Update payfee.php

--- Mini Program/payfee.php
+++ Mini Program/payfee.php
@@ -10,12 +10,10 @@ $mch_id=''; //微信支付商户支付号
 $key=''; //Api 密钥
 $out_trade_no = date('YmdHis').rand(100,999);
 $total_fee = $_POST['fee'];
-if(empty($total_fee)) //押金
-{
+if (empty($total_fee)) {  //押金
     $body = "充值押金";
     $total_fee = floatval(99*100);
-}
- else {
+} else {
      $body = "充值余额";
      $total_fee = floatval($total_fee*100);
  }
